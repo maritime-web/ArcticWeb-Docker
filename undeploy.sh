087035261e63d483fac5294c1bd19b0d700f270c
Oliver Steensen-Bech Haagh <osh@dma.dk>
2016-06-29 15:17:13 +0200

    minor correction

--- undeploy.sh
+++ undeploy.sh
@@ -6,7 +6,7 @@ docker stop arcticweb arctic_db arctic_couch arctic_watchtower arctic_nginx
 full ()
 {
     echo "Removing containers"
-    docker rm arcticweb arctic_db arctic_couch arctic_watchtower
+    docker rm arcticweb arctic_db arctic_couch arctic_watchtower arctic_nginx
     docker-compose -f logging/docker-compose.yml down
     docker network rm arcticnet
 }
